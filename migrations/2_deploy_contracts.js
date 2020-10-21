55f23f94e61a134d27c5ed878cd035dac2df1a06
MXmaster2s <bullzeye711@gmail.com>
2020-10-21 13:30:15 +0530

    Adding Crowdsale contract of OZ

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
-var MyToken = artifacts.require("./MyToken.sol");
+var MyToken = artifacts.require("./MyToken");
+var MyTokenSale = artifacts.require("MyTokenSale");
 
 module.exports = async function(deployer) {
+  let address = await web3.eth.getAccounts();
   await deployer.deploy(MyToken, 100000);
+  await deployer.deploy(MyTokenSale, 1, address[0], MyToken.address);
+  let instance = await MyToken.deployed();
+  await instance.transfer(MyTokenSale.address, 100000);
 };
